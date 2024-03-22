5e5152220687f5b359ab29a3621cbd92dfb65f79
YieldRay <yieldray@outlook.com>
2024-03-22 14:12:28 +0800

    bump up dependencies

--- src/utils/translate.ts
+++ src/utils/translate.ts
@@ -4,19 +4,16 @@ export default async function translate(md: string) {
     if (cache.has(md)) return cache.get(md)!
 
     try {
-        const translated = await fetch(
-            'https://yieldray-geminitranslatesimple.web.val.run/',
-            {
-                body: new URLSearchParams({ md }),
-                method: 'POST',
-            }
-        ).then((res) => res.text())
+        const translated = await fetch('https://gnm.deno.dev/', {
+            body: new URLSearchParams({ md }),
+            method: 'POST',
+        }).then((res) => res.text())
 
         cache.set(md, translated)
         return translated
     } catch {
         throw new Error(
-            '无法翻译，请关闭翻译开关！原因：接口受限，请稍后再试（可以尝试使用 https://yieldray-html2md.web.val.run/ ）'
+            '无法翻译，请关闭翻译开关！原因：接口受限，请稍后再试（可尝试使用 https://yieldray-html2md.web.val.run/ ）'
         )
     }
 }
